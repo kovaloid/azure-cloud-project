7ab49d55a463fdb91d4ad58f3f3178023b9ffb04
kovaloid <kov3000@ya.ru>
2017-12-03 23:39:16 +0300

    Handle static content

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    var page = url.parse(request.url).pathname;
+    console.log(page);
+    
+    if (page == '/') {
+	    response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end('You\'re at the reception desk. How can I help you?');
+    } else if (page == '/basement') {
+	    response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end('You\'re in the wine cellar. These bottles are mine!');
+    } else if (page == '/floor/1/bedroom') {
+	    response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write('Hey, this is a private area!');
+		response.end();
+    } else {
+
+	    pathName =url.parse(request.url).pathname;
+        fs.readFile(__dirname + pathName, function(err, data) {
+          if (err) {
+              response.writeHead(404, {'Content-type':'text/plain'});
+              response.write('Page Was Not Found');
+              response.end();
+			  console.log(err);
+          } else {
+		     console.log(data);
+              response.writeHead(200, {'Content-type':'text/html'});
+              response.write(data);
+              response.end();
+	      }
+    	});
+	
+	
+	}
 
 });
 
