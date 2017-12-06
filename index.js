5e10886b71ba8d50df8fb1be2930c423d5dae109
kovala6 <kov3000@ya.ru>
2017-12-06 13:32:45 +0300

    Add tabs

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var server = http.createServer(function(request, response) {
     console.log(request.method + ' ' + request.url);
     
     if (request.method === "GET") {
-    
+        
         if (request.url === '/') {
-		
+            
             fs.readFile(__dirname + '/index.html', function (error, data) {
                 if (error) {
                     response.writeHead(404, {'Content-type':'text/plain'});
@@ -23,82 +23,66 @@ var server = http.createServer(function(request, response) {
                 }
             });
             
-       	} else if (page == '/basement') {
-		
+        } else if (page == '/about') {
+        
             response.writeHead(200, {"Content-Type": "text/plain"});
-            response.end('You\'re in the wine cellar. These bottles are mine!');
-			
-        } else if (page == '/floor/1/bedroom') {
-		
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write('Hey, this is a private area!');
-            response.end();
+            response.end('It\'s a course work of Artem Kovalev');
 
         } else {
-			var pathName = url.parse(request.url).pathname;
+
+            var pathName = url.parse(request.url).pathname;
             fs.readFile(__dirname + pathName, function (error, data) {
                 if (error) {
-					response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
+                    response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
                     response.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
                 } else {
-					var ext = pathName.substr(pathName.lastIndexOf('.') + 1);
-					if (ext === 'js') {
-						response.writeHead(200, {'Content-type':'application/javascript'});
-                        //response.write(data);
-                        //response.end();
-					} else if (ext === 'css') {
-						response.writeHead(200, {'Content-type':'text/css'});
-                        //response.write(data);
-                        //response.end();
-					} else {
-						//console.log(data);
+                    var ext = pathName.substr(pathName.lastIndexOf('.') + 1);
+                    if (ext === 'js') {
+                        response.writeHead(200, {'Content-type':'application/javascript'});
+                    } else if (ext === 'css') {
+                        response.writeHead(200, {'Content-type':'text/css'});
+                    } else {
                         response.writeHead(200, {'Content-type':'text/html'});
-                        //response.write(data);
-                        //response.end();
-					}
-					response.write(data);
+                    }
+                    response.write(data);
                     response.end();
-					
                 }
             });
+
         }
 
-    
     } else if (request.method === "POST") {
     
-        if (page == '/handle') {
+        if (page == '/feedback') {
         
             var requestBody = '';
             request.on('data', function(data) {
                 requestBody += data;
                 if(requestBody.length > 1e7) {
-                response.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
-                response.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
+                    response.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
+                    response.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
                 }
             });
             request.on('end', function() {
-                var formData = querystring.parse(requestBody);
-                console.log(formData);
+                var formData = querystring.parse(requestBody),
+				    formDataKey = JSON.parse(Object.keys(formData)[0]);
+				console.log(formDataKey);
                 response.writeHead(200, {'Content-Type': 'text/html'});
                 response.write('<!doctype html><html><head><title>response</title></head><body>');
-                response.write('Thanks for the data!<br />User Name: '+formData.UserName);
-                response.write('<br />Repository Name: '+formData.Repository);
-                response.write('<br />Branch: '+formData.Branch);
+                response.write('Thanks for the data!<br />User: '+formDataKey.user);
+                response.write('<br />Message: '+formDataKey.message);
                 response.end('</body></html>');
               });
         
         } else {
             response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
             response.end('<!doctype html><html><head><title>404</title></head><body>404: Resource Not Found</body></html>');
-		}
-    
+        }
     
     } else {
         response.writeHead(405, 'Method Not Supported', {'Content-Type': 'text/html'});
         return response.end('<!doctype html><html><head><title>405</title></head><body>405: Method Not Supported</body></html>');
     }
-	
-	
 
 });
 
---
+++ main.service.js
@@ -0,0 +1,31 @@
+angular
+    .module('Main')
+	.factory('MainService', ['$q', '$http',
+	    function MainService($q, $http) {
+			
+			function getAbout() {
+				var deferred = $q.defer();
+				$http.get('/about').then(function (response) {
+					deferred.resolve(response);
+				}, function (error) {
+					deferred.reject(error);
+				});
+				return deferred.promise;
+			}
+			
+			function sendFeedback(data) {
+				var deferred = $q.defer();
+				$http.post('/feedback', data).then(function (response) {
+					deferred.resolve(response);
+				}, function (error) {
+					deferred.reject(error);
+				});
+				return deferred.promise;
+			}
+			
+			return {
+				getAbout: getAbout,
+				sendFeedback: sendFeedback
+			}
+		
+        }]);
